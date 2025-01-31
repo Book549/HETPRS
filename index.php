72d508e9b3b3923393b70de6be675e711686f8e7
Wattanachai Maijampa <wmaijampa549@gmail.com>
2025-01-31 23:49:41 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h2>ชื่อแอป: Hospital Emergency Triage & Patient Record System (HETPRS)</h2>
+	<h2>หรือชื่อที่กระชับขึ้น: Emergency Triage & Patient Record System (ETPRS)</h2>
+	<hr>
+	<p>คำอธิบาย:
+	แอปนี้ช่วยให้เจ้าหน้าที่โรงพยาบาลสามารถ บันทึกข้อมูลส่วนตัวและอาการของผู้ป่วยฉุกเฉิน ได้อย่างรวดเร็ว พร้อมระบบ วิเคราะห์และจำแนกกลุ่มสีตามระดับความฉุกเฉิน (เช่น สีเขียว, เหลือง, แดง, ดำ) ตามมาตรฐานการแพทย์ แทนการใช้กระดาษ ซึ่งช่วยลดข้อผิดพลาด เพิ่มความแม่นยำ และทำให้บุคลากรทางการแพทย์สามารถตัดสินใจได้รวดเร็วขึ้นในสถานการณ์ฉุกเฉิน</p>
+</body>
+</html>
